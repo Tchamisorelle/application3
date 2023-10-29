afdb9819b63c80ab2aec0c8e2ea1f44afbec50e7
Tchamisorelle <sorelletamen8@gmail.com>
2023-10-29 18:56:44 +0100

    appel des deux methodes de la class division

--- testdivison/TestDivision.java
+++ testdivison/TestDivision.java
@@ -6,6 +6,11 @@ import inf331.application3.defclass.Division;
 public class TestDivision {
     public static void main(String[] agrs){
         Division div = new Division();
+        double testmethod1 = div.diviser(9, 3);
+        System.out.println("le resultat de la methode 1 est " + testmethod1);
+
+        double testmethod2 = Division.diviserStatique(30, 4);
+        System.out.println("le resultat de la methode 2 est " + testmethod2);
     }
     
 }
